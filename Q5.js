62d3c1fbc25714965395ec59b3c005db2df0a3cd
yashkdm01 <160143421+yashkdm01@users.noreply.github.com>
2024-02-15 19:31:34 +0530

    Add files via upload

---
+++ Q5.js
@@ -0,0 +1,5 @@
+const principal = 1000
+const rate = 5
+const time = 2 // in years
+const result = (principal * rate * time)/100
+console.log("Simple Interest =", result)
---
+++ Q6.js
@@ -0,0 +1,4 @@
+const height = 160 // in cm
+const weight = 55 // in kg
+const BMI = weight/ height * height
+console.log("BMI =", BMI)
